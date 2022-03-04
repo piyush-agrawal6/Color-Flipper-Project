f203131f37e6210d6330471f480167312587e061
piyush-agrawal6 <agrawaljoy1@gmail.com>
2022-03-04 19:37:52 +0530

    Color Flipper

---
+++ simple.js
@@ -0,0 +1,14 @@
+const colors = ["green", " red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+  const randomNum = getRandomNum();
+  document.querySelector(".container").style.backgroundColor =
+    colors[randomNum];
+  color.textContent = colors[randomNum];
+});
+function getRandomNum() {
+  return Math.floor(Math.random() * colors.length);
+}
